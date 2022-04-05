19c29e876d1df7e3d036bb5485126633b6d154c8
matti3r <matti3r@protonmail.com>
2022-04-04 22:04:37 -0500

    Final Commit for submission

--- mricketts-hw3-code/Graph.h
+++ mricketts-hw3-code/Graph.h
@@ -1,9 +1,10 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <iostream>
 #include <string>
+#include <vector>
 
+// https://favtutor.com/blogs/dijkstras-algorithm-cpp#:~:text=Algorithm%20for%20Dijkstra's%20in%20C%2B%2B&text=Consider%20source%20vertex%20as%20current,replace%20it%20otherwise%20ignore%20it
 
 using namespace std;
 
@@ -30,6 +31,9 @@ public:
 	 */
 	Graph() 
 	{
+		for (int k = 0; k <= MAXV; k++) {
+			edges[k] = new EdgeNode("", k, 0, nullptr);
+		}
 	}
 
 	/**
@@ -87,20 +91,28 @@ public:
 	void deleteEdge(int i, int j) {
 
 		// Temp EdgeNode to scan i's destinations
-		EdgeNode* temp = edges[i]->next;
+		EdgeNode* temp = edges[i];
 
 		// Scan all of the destination nodes of i for j
 		while (temp->next != nullptr) {
+
+			unsigned int cnt = 0;
+
 			if (temp->next->y == j) {
 
 				// Bypass the previous connection
 				EdgeNode* toDelete = temp->next;
-				temp->next == temp->next->next;
+				temp->next = temp->next->next;
 
 				// Deallocate the memory of this edge and instance of j
 				delete toDelete;
-				
+
 				// Link to j found, break the loop
+				break;
+			}
+
+			if (cnt == 20){
+				cout << "\nLink does not exist\n";
 				break;
 			}
 
@@ -117,6 +129,8 @@ public:
 	void dump() {
 
 		// PRINT ALL EDGENODES
+		cout << "From:\tTo:\tWeight:\n";
+
 		// For every source node
 		for (int i = 0; i <= MAXV; i++) {
 
@@ -125,7 +139,7 @@ public:
 
 			// Look through all its destination nodes
 			while (temp != nullptr) {
-				cout << edges[i]->name << ", " << temp->name << ", " << temp->weight << endl;
+				cout << edges[i]->name << "(" << edges[i]->y << "),\t" << temp->name << "(" << temp->y << "),\t" << temp->weight << endl;
 
 				temp = temp->next;					// Check the next node's destination
 			}
@@ -135,26 +149,83 @@ public:
 
 	/**
 	 * Print the shortest path from s to g
+	 * Uses Dijkstra's single source shortest path algorithm
 	 */
-	void path(int s, int g) {
+	void path(int s, int g)
+	{
+		int dist[MAXV+1];			// The output array. dist[k] will hold the shortest distance from src to k
+		int prev[MAXV+1];			// Array to hold the node right before the current node gets finalized
+		bool finalized[MAXV+1];		// nodeProcessed[k] will be true if vertex k is included in shortest path tree or shortest distance from src to k is finalized
 
 
-		/*
-			*********        *********        *         *                *                		
-		    *                    *            *         *                *
-		    *					 *            *         *                *
-		    *					 *            *         *                *
-		    *********			 *            *         *                *
-		            *			 *            *         *                *
-		            *			 *            *         *                *
-		            *			 *            *         *                *
-		    *********			 *            *         *********        *********
-		*/
+		// Initialize all distances as INFINITE and finalized[] as false
+		for (int k = 0; k <= MAXV; k++) {
+			dist[k] = INT_MAX;
+			prev[k] = -1;
+			finalized[k] = false;
+		}
 
+		// Distance of source vertex from itself is always 0
+		dist[s] = 0;
 
+		// Find shortest path for all vertices
+		for (int k = 0; k < MAXV; k++) {
 
+			// Pick the minimum distance vertex from the set of vertices not yet processed. u is always equal to src in the first iteration.
+			int u = minDistance(dist, prev, finalized);
+
+			// Mark the picked vertex as processed
+			finalized[u] = true;
+
+			// Update dist value of the adjacent vertices of the picked vertex.
+			EdgeNode* temp = edges[u];
+			while (temp) {
+
+				// Update dist[v] only if is not in nodeProcessed, there is an edge from u to v, and total weight of path from src to v through u is smaller than current value of dist[v]
+				if (!finalized[temp->y] && dist[u] != INT_MAX && dist[u] + temp->weight < dist[temp->y]) {
+					dist[temp->y] = dist[u] + temp->weight;
+				}
+					
+				if (temp->next == nullptr)
+					prev[temp->y] = temp->y;
+
+				temp = temp->next;
+			}
+		}
+
+		// print the constructed distance array
+		printSolution(dist);
+
+		// Print the shortest path
+		cout << "\nThe shortest path from " << s << " to " << g << " is: " << dist[g] << endl << endl;
 	}
-	
+
+	// Function to find the vertex with minimum distance value from the set of vertices not yet included in shortest path tree
+	int minDistance(int dist[], int prev[], bool finalized[])
+	{
+
+		// Initialize min value
+		int min = INT_MAX, min_index;
+
+		for (int k = 0; k <= MAXV; k++)
+			if (finalized[k] == false && dist[k] <= min)
+				min = dist[k], min_index = k;
+
+		return min_index;
+	}
+
+	// Debugger, print constructed distance array
+	void printSolution(int dist[])
+	{
+		cout << "Vertex \t Distance from Source" << endl;
+		for (int i = 0; i <= MAXV; i++)
+			if (dist[i] < INT_MAX)
+				cout << edges[i]->name << " \t\t" << dist[i] << endl;
+	}
+
+
+
+
 	/**
 	 * Prints all the "dead end" nodes in the network. A node is a “dead end” if it has an out-degree of zero.  
 	 */
@@ -163,7 +234,7 @@ public:
 		// Look for any source node in the array that does not have any destination nodes
 		for (int i = 0; i <= MAXV; i++)
 			if (edges[i]->next == nullptr)
-				cout << (edges[i]->name);
+				cout << edges[i]->y << ", " << edges[i]->name << endl;
 
 	}
 
--- mricketts-hw3-code/Source.cpp
+++ mricketts-hw3-code/Source.cpp
@@ -1,11 +1,165 @@
+
+
+#include <iostream>
+#include <string>
+
 #include "Graph.h"
 
 
+// Function declaration
+string toLower(string);
+void debugFillGraph(Graph);
+
+// Main function
 int main() {
 
+	
 	Graph graph = Graph();
 
-	//TODO: Implement menu here
+	debugFillGraph(graph);
 
+	string userMenuOption;
+
+	cout << "\n\n\nThis program implements a graph.\n";
+
+	do {
+
+		cout << "\n\nWhat would you like to do:\n"
+				"A: Add an edge.\n" <<
+				"D: Delete an edge.\n" <<
+				"N: Add a name to a node.\n" <<
+				"P: Print all edges in network.\n" <<
+				"SP: Find shortest path between two nodes.\n" <<
+				"DE: List all the dead-end nodes.\n" <<
+				"IN: List all the inaccessible nodes.\n" <<
+				"E: exit\n\n";
+
+		// Get user input
+		cin >> userMenuOption;
+
+		// Implement menu
+		if ("a" == toLower(userMenuOption)) {
+			int from, to, weight;
+
+			// Get the nodes to link
+			cout << "Add Edge\n";
+			cout << "From Node: "; cin >> from;
+			cout << "To Node: "; cin >> to;
+			cout << "Weight: "; cin >> weight;
+
+
+			// Link the nodes
+			graph.link(from, to, weight);
+
+			cout << "Edge Added";
+
+		}
+		else if ("d" == toLower(userMenuOption)) {
+			int from, to;
+			
+			cout << "Remove Edge\n";
+			cout << "From Node: "; cin >> from;
+			cout << "To Node: "; cin >> to;
+
+			graph.deleteEdge(from,to);
+
+			cout << "Edge Removed";
+		}
+		else if ("n" == toLower(userMenuOption)) {
+			int node;
+			string name;
+
+			cout << "Add Name to Node\n";
+			cin.ignore();
+			cout << "Name: "; getline(cin, name);
+			cout << "Node: "; cin >> node;
+
+			graph.name(node, name);
+
+			cout << "Name Added";
+		}
+		else if ("p" == toLower(userMenuOption)) {
+			graph.dump();
+		}
+		else if ("sp" == toLower(userMenuOption)) {
+			int from, to;
+
+			cout << "Find the Shortest Path\n";
+			cout << "From Node: "; cin >> from;
+			cout << "To Node: "; cin >> to;
+
+			graph.path(from,to);
+		}
+		else if ("de" == toLower(userMenuOption)) {
+			graph.dend();
+		}
+		else if ("in" == toLower(userMenuOption)) {
+			graph.inacess();
+		}
+		else if ("e" != toLower(userMenuOption)) {
+			cout << "Invalid Input";
+		}
+
+
+	} while (toLower(userMenuOption) != "e");
+
+	
 	return 0;
+}
+
+
+/**
+*  Function to convert a string to lowercase
+*  Param: toConvert, String to convert to lowercase
+*/ 
+
+string toLower(string toConvert) {
+
+	for (unsigned int i = 0; i < toConvert.length(); i++)
+	{
+		toConvert[i] = tolower(toConvert[i]);
+	}
+
+	return toConvert;
+}
+
+
+void debugFillGraph(Graph graph) {
+	graph.name(0, "A");
+	graph.name(1, "B");
+	graph.name(2, "C");
+	graph.name(3, "D");
+	graph.name(4, "E");
+	graph.name(5, "F");
+	graph.name(6, "G");
+
+	graph.link(0, 1, 6);
+	graph.link(0, 5, 10);
+	graph.link(0, 6, 5);
+
+	graph.link(1, 0, 6);
+	graph.link(1, 5, 1);
+	graph.link(1, 2, 9);
+
+	graph.link(2, 1, 9);
+	graph.link(2, 5, 7);
+	graph.link(2, 3, 8);
+
+	graph.link(3, 2, 8);
+	graph.link(3, 4, 4);
+
+	graph.link(4, 3, 4);
+	graph.link(4, 2, 1);
+	graph.link(4, 5, 7);
+	graph.link(4, 6, 8);
+
+	graph.link(5, 0, 10);
+	graph.link(5, 1, 1);
+	graph.link(5, 2, 7);
+	graph.link(5, 4, 7);
+	graph.link(5, 6, 3);
+
+	graph.link(6, 1, 5);
+	graph.link(6, 5, 3);
+	graph.link(6, 4, 8);
 }
